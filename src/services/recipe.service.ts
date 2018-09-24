d483b7e238b9ef8544694830d9a935260d61b472
Paula <paulagarizcun@gmail.com>
2018-09-24 09:59:40 +0200

    ajustes

--- src/services/recipe.service.ts
+++ src/services/recipe.service.ts
@@ -8,24 +8,10 @@ export class RecipeService {
   private urlPuppy = "/api/?q="
 
   constructor(private http : HttpClient) { }
-  // getPages() {
-  //   let result = [];
-  //   let page = 1;
-  //   this.http.get(`/api/?p=${page}`)
-  //     .pipe(map(res => {
-  //       result.push(res[`results`])
-  //       if(page < 20){
-  //         return page++
-  //       }
-  //     }))
-  //   return result;
-  // }
+
   getRecipes() {
-    console.log("Recetas listas!")
     return this.http.get(this.urlPuppy)
-      .pipe(map(res => {
-        console.log(res)
-        return res}));
+      .pipe(map(res => res));
   }
 
 }
